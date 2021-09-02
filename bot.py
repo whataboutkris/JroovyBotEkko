0c53ecd7b6f6040f970daf440951f4fecca65dde
Kristian Losenara <whataboutkris@gmail.com>
2021-09-02 16:50:04 -0700

    init

---
+++ bot.py
@@ -0,0 +1,27 @@
+"""
+push and pull test
+Boilerplate taken from
+https://realpython.com/how-to-make-a-discord-bot-python/#what-is-a-bot
+"""
+
+import os
+from dotenv import load_dotenv
+
+import discord
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+bot = commands.Bot(command_prefix="-")
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+@bot.command(name='test')
+async def test_bot(messageBox):
+    response = "This is a test."
+    await messageBox.send(response)
+
+bot.run(TOKEN)
